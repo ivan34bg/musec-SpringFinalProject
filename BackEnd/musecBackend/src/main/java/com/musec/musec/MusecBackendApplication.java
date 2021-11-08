be18446f67c7f84c44abbf748d8510bd5fbcb8ee
Ivan Dinkov <ivandinkov2001@gmail.com>
2021-11-08 19:07:36 +0200

    Created backend using spring

---
+++ BackEnd/musecBackend/src/main/java/com/musec/musec/MusecBackendApplication.java
@@ -0,0 +1,13 @@
+package com.musec.musec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusecBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusecBackendApplication.class, args);
+    }
+
+}
---
+++ BackEnd/musecBackend/src/test/java/com/musec/musec/MusecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musec.musec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusecBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
